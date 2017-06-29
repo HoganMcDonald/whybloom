142fae9be11db1d84c81704971ef3c673ffe8e40
Hogan McDonald <hpeter.mcdonald@icloud.com>
2017-06-29 09:47:08 -0500

    pushing changes that weren't commited

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,20 +1,27 @@
 $(document).ready(function() {
+
   //scroll function - paralax
   $(window).scroll(function() {
     var wScroll = $(this).scrollTop();
     $('.wrapper').css({
-      'transform': 'translate(0px, -' + wScroll / 2 + '%)'
+      'transform': 'translate(0px, -' + wScroll / 4 + '%)'
     }); //end wrapper listener
 
     //display link images
     if (wScroll > $('#links').offset().top - $(window).height() / 1.1) {
-
+      //loop through images on homepage
       $('.link').each(function(i) {
         setTimeout(function() {
           $('.link').eq(i).addClass('reveal');
         }, 75 * (i + 1));
-      });
+      }); //end image loop
+    } //end fade in link images
 
-    }
+    //fade in coffee logo
+    if (wScroll > $('.coffee').offset().top - $(window).height() / 1.1) {
+      $('.coffee').addClass('reveal');
+      console.log('coffee hit');
+    } //end coffee fade function
   }); //end scroll function
+
 }); //end document ready
